7d6082704f78aec4dfb333840aabe3e8674b9fc9
Samuel Barney <samjbarney@gmail.com>
2015-07-10 18:43:08 -0600

    Added recipe for Murky Water Bucket

--- Recipes.lua
+++ Recipes.lua
@@ -2,13 +2,30 @@ wasteland_Recipes = {}
 
 local function setup()
 	local stick = cItem(E_ITEM_STICK)
+	local seeds = w_Items['wasteland:seed']
 
+	-- Planks
 	local tmp = CraftingRecipe_new(2,2)
 	CraftingRecipe_SetResult(tmp, w_Items['wasteland:dry_planks'])
 	CraftingRecipe_SetItem(tmp, 0,0, stick)
 	CraftingRecipe_SetItem(tmp, 0,1, stick)
 	CraftingRecipe_SetItem(tmp, 1,0, stick)
 	CraftingRecipe_SetItem(tmp, 1,1, stick)
+	table.insert(wasteland_Recipes, tmp)
+
+	-- Water Bucket
+	tmp = CraftingRecipe_new(3,3)
+	CraftingRecipe_SetResult(tmp, w_Items['wasteland:murky_water_bucket'])
+	CraftingRecipe_SetItem(tmp,0,0, seeds)
+	CraftingRecipe_SetItem(tmp,1,0, seeds)
+	CraftingRecipe_SetItem(tmp,2,0, seeds)
+	CraftingRecipe_SetItem(tmp,0,1, seeds)
+	CraftingRecipe_SetItem(tmp,1,1, cItem(E_ITEM_BUCKET))
+	CraftingRecipe_SetItem(tmp,2,1, seeds)
+	CraftingRecipe_SetItem(tmp,0,2, seeds)
+	CraftingRecipe_SetItem(tmp,1,2, seeds)
+	CraftingRecipe_SetItem(tmp,2,2, seeds)
+
 
 	table.insert(wasteland_Recipes, tmp)
 end
