0b6b9272fa09785fbd131c2d08d7b8d62a9f84aa
Samuel Barney <samjbarney@gmail.com>
2015-07-10 18:41:23 -0600

    Added Murky Water Bucket

--- Items.lua
+++ Items.lua
@@ -2,3 +2,4 @@ w_Items = {}
 
 w_Items['wasteland:seed'] = cItem(E_ITEM_SEEDS, 1, 0, '', 'Unknown Seeds', 'Who knows what this will grow')
 w_Items['wasteland:dry_planks'] = cItem(E_BLOCK_PLANKS, 1, E_META_PLANKS_BIRCH, '', 'Sunbleached Planks')
+w_Items['wasteland:murky_water_bucket'] = cItem(E_ITEM_WATER_BUCKET, 1, 0, '', 'Murky Water Bucket')
